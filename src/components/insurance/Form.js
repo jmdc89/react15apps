374045629355f7a21e6a30f92ff20de0d9b533b4
Jose Manuel <jmdcarq89@gmail.com>
2021-03-25 19:10:12 +0100

    helper

--- src/components/insurance/Form.js
+++ src/components/insurance/Form.js
@@ -79,38 +79,38 @@ const Form = () => {
 
         guardarError(false);
 
-        // Una base de 2000
-        let resultado = 2000;
+        // // Una base de 2000
+        // let resultado = 2000;
 
-        // obtener la diferencia de años
-        const diferencia = obtenerDiferenciaYear(year);
+        // // obtener la diferencia de años
+        // const diferencia = obtenerDiferenciaYear(year);
 
-        // por cada año hay que restar el 3%
-        resultado -= (( diferencia * 3 ) * resultado) / 100;
+        // // por cada año hay que restar el 3%
+        // resultado -= (( diferencia * 3 ) * resultado) / 100;
 
-        // Americano 15
-        // Asiatico 5%
-        // Europeo 30%
-        resultado = calcularMarca(marca) * resultado;
+        // // Americano 15
+        // // Asiatico 5%
+        // // Europeo 30%
+        // resultado = calcularMarca(marca) * resultado;
 
-        // Basíco aumenta 20%
-        // Completo 50%
-        const incrementoPlan = obtenerPlan(plan);
-        resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
+        // // Basíco aumenta 20%
+        // // Completo 50%
+        // const incrementoPlan = obtenerPlan(plan);
+        // resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
 
-        guardarCargando(true);
+        // guardarCargando(true);
 
-        setTimeout(() => {
+        // setTimeout(() => {
 
-            // Elimina el spinner
-            guardarCargando(false);
+        //     // Elimina el spinner
+        //     guardarCargando(false);
 
-            // pasa la información al componente principal
-            guardarResumen({
-                cotizacion: Number(resultado),
-                datos
-            });
-        }, 3000);
+        //     // pasa la información al componente principal
+        //     guardarResumen({
+        //         cotizacion: Number(resultado),
+        //         datos
+        //     });
+        // }, 3000);
 
     }
 
