6f26783e78a291a17d4538021f9a4126739c436e
Jose Manuel <jmdcarq89@gmail.com>
2021-03-25 18:56:22 +0100

    Form styles

--- src/components/insurance/Form.js
+++ src/components/insurance/Form.js
@@ -1,22 +1,69 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+    line-height: 0px;
+    
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width:100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
 const Form = () => {
     return (
         <form>
-            <div>
-                <label>Marca</label>
-                <select>
+            <Campo>
+                <Label>Marca</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                     <option value="americano">Americano</option>
                     <option value="europeo">Europeo</option>
                     <option value="asiatico">Asiatico</option>
-                </select>
-            </div>
+                </Select>
+            </Campo>
 
-            <div>
-                <label>Año</label>
-                <select
+            <Campo>
+                <Label>Año</Label>
+                <Select
                     name="year"
                     // value={year}
                     // onChange={obtenerInformacion}
@@ -32,12 +79,12 @@ const Form = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                </select>
-            </div>
+                </Select>
+            </Campo>
 
-            <div>
-                <label>Plan</label>
-                <input
+            <Campo>
+                <Label>Plan</Label>
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="basico"
@@ -45,16 +92,16 @@ const Form = () => {
                     // onChange={obtenerInformacion}
                 /> Básico
 
-                <input
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="completo"
                     // checked={plan === "completo"}
                     // onChange={obtenerInformacion}
                 /> Completo
-            </div>
+            </Campo>
 
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
 
         </form>
     );
