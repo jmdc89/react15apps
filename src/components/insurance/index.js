0447bf4a12724f4516bddbc7cf82fd3471c5fdb4
Jose Manuel <jmdcarq89@gmail.com>
2021-03-25 18:45:46 +0100

    Form structure

--- src/components/insurance/index.js
+++ src/components/insurance/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from './Header';
+import Form from './Form';
+import styled from 'styled-components/macro';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 const Insurance = () => {
     return (
-        <Header
+        <Contenedor>
+           <Header
             titulo='Cotizador de Seguros'
-        />
+            /> 
+
+            <ContenedorFormulario>
+                <Form />
+            </ContenedorFormulario>
+        </Contenedor>
+        
     );
 }
  
