e0ed4fe507ba553f25ee80f9700e88c32f3cb45f
Jose Manuel <jmdcarq89@gmail.com>
2021-03-25 18:27:03 +0100

    file structure

---
+++ src/components/insurance/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Insurance = () => {
+    return (
+        <h1>Cotizador</h1>
+    );
+}
+ 
+export default Insurance;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {GlobalStyles} from './global-styles';
 // import Admin from "./components/adminpacientes";
-import Budget from "./components/budget";
+// import Budget from "./components/budget";
+import Insurance from "./components/insurance";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     {/* <Admin/> */}
-    <Budget/>
+    {/* <Budget/> */}
+    <Insurance/>
   </React.StrictMode>,
   document.getElementById('root')
 );
