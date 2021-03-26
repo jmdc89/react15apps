b67e29c3f86377c0026561a1398da836ac66d594
Jose Manuel <jmdcarq89@gmail.com>
2021-03-26 10:55:45 +0100

    Breaking bad index styles

---
+++ src/components/bbquote/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+` ;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`;
+
+const BreakingBad = () => {
+    return (
+        <h1>Breaking bad</h1>
+    );
+}
+ 
+export default BreakingBad;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import {GlobalStyles} from './global-styles';
 // import Admin from "./components/adminpacientes";
 // import Budget from "./components/budget";
-import Insurance from "./components/insurance";
+// import Insurance from "./components/insurance";
+import BreakingBad from "./components/bbquote";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     {/* <Admin/> */}
     {/* <Budget/> */}
-    <Insurance/>
+    {/* <Insurance/> */}
+    <BreakingBad/>
   </React.StrictMode>,
   document.getElementById('root')
 );
