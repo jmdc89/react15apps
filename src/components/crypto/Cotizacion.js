1d2f29a1018adf6448785884c740b218deba2876
Jose Manuel <jmdcarq89@gmail.com>
2021-03-30 23:40:05 +0200

    Cotizacion style

--- src/components/crypto/Cotizacion.js
+++ src/components/crypto/Cotizacion.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+    span {
+        font-weight:bold;
+    }
+`;
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight:bold;
+    }
+`;
+
 const Cotizacion = ({resultado}) => {
     if(Object.keys(resultado).length === 0) return null;
 
